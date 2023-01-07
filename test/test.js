bf2ea32c00a22feb9b060b321eecb9e571ade9d8
Docfile <achachia2003@yahoo.fr>
2023-01-07 12:28:48 +0100

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,4 +1,6 @@
 const puppeteer = require('puppeteer');
+const { join } = require('path');
+let fs = require('fs');
 
 (async () => {
   const browser = await puppeteer.launch();
@@ -7,6 +9,22 @@ const puppeteer = require('puppeteer');
     waitUntil: 'networkidle2',
   });
   await page.pdf({ path: 'test.pdf', format: 'a4' });
+  
+  let student = { 
+    name: 'Mike',
+    age: 23, 
+    gender: 'Male',
+    department: 'English',
+    car: 'Honda' 
+};
+
+  
+fs.writeFile ("input.json", JSON.stringify(student), function(err) {
+    if (err) throw err;
+    console.log('complete');
+    console.log(fs.readFileSync("input.json", "utf8"));
+} 
+);
 
   await browser.close();
 })();
